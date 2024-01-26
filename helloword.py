0082d140a6693ae0b633af47ec6e50c5b12376a2
huu quang <huuquang1511@gmail.com>
2024-01-26 16:10:50 +0700

    them file br quang

---
+++ helloword.py
@@ -0,0 +1,12 @@
+variable1 = 1
+variable2 = 2
+variable3 = 3
+variable4 = 4
+variable5 = new new
+
+# In giá trị của các biến
+print("Biến 1:", variable1)
+print("Biến 2:", variable2)
+print("Biến 3:", variable3)
+print("Biến 4:", variable4)
+print("Biến 5:", variable5)
