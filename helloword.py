c94774e5c3c345a910275a9c7a631c4bfb23678b
quan442644 <quancab1508@gmail.com>
2024-01-26 16:16:32 +0700

    upload

---
+++ helloword.py
@@ -0,0 +1,14 @@
+Y# Khai báo 5 biến số tự nhiên từ 1 đến 5
+
+
+variable1 = 1
+variable2 = 2
+variable3 = 3
+variable4 = 4
+variable5 = 2307
+# In giá trị của các biến
+print("Biến 1:", variable1)
+print("Biến 2:", variable2)
+print("Biến 3:", variable3)
+print("Biến 4:", variable4)
+print("Biến 5:", variable5)
