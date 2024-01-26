60d09411df81ba66df131108598c6bd54c57b93e
tanluong <luongvonhattan11t1@gmail.com>
2024-01-26 23:03:02 +0700

    doi file helloword

--- helloword.py
+++ helloword.py
@@ -1,18 +1,18 @@
-
 # Khai báo 5 biến số tự nhiên từ 1 đến 5
 
 variable1 = 1
 variable2 = 2
 variable3 = 3
 variable4 = 4
-variable5 = abc
-variable4 = new new new
-variable5 = 5
+variable5 = 5  # Giữ nguyên giá trị ở nhánh master/main
+# variable4 = new new new  # Bỏ comment ở nhánh feature
+# variable5 = abc  # Bỏ comment ở nhánh feature
+variable4 = "new new new"  # Sửa giá trị ở nhánh feature
+variable5 = "abc"  # Sửa giá trị ở nhánh feature
+
 # In giá trị của các biến
 print("Biến 1:", variable1)
 print("Biến 2:", variable2)
 print("Biến 3:", variable3)
 print("Biến 4:", variable4)
 print("Biến 5:", variable5)
-
-
