0fafac9be0d2ca0cdd148e0f4cf1fe6f1c764de3
tanluong <luongvonhattan11t1@gmail.com>
2024-01-26 23:39:22 +0700

    doi file helloword

--- helloword.py
+++ helloword.py
@@ -4,8 +4,12 @@ variable1 = 1
 variable2 = 2
 variable3 = 3
 variable4 = 4
-variable5 = abc
-variable5 = def
+variable5 = 5  # Giữ nguyên giá trị ở nhánh master/main
+# variable4 = new new new  # Bỏ comment ở nhánh feature
+# variable5 = abc  # Bỏ comment ở nhánh feature
+variable4 = "new new new"  # Sửa giá trị ở nhánh feature
+variable5 = "abc"  # Sửa giá trị ở nhánh feature
+
 # In giá trị của các biến
 print("Biến 1:", variable1)
 print("Biến 2:", variable2)
